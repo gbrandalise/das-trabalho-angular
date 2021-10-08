c47f812578640d82463a4d6e5805af5706d7fe1c
joaofelipe <joaofelipe1294@gmail.com>
2021-10-07 21:41:53 -0300

    feat: add insert order method.

--- backend/src/main/java/br/com/ufpr/das/order/OrderService.java
+++ backend/src/main/java/br/com/ufpr/das/order/OrderService.java
@@ -1,6 +1,10 @@
 package br.com.ufpr.das.order;
 
+import java.util.Set;
+
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
 
 import org.springframework.stereotype.Service;
 
@@ -21,7 +25,7 @@ public class OrderService {
   }
 
   private OrderDTO save(OrderDTO order) {
-    // this.validate(order);
+    this.validate(order);
     Order orderEntity = OrderMapper.INSTANCE.toEntity(order);
     orderEntity = this.orderRepository.save(orderEntity);
     return OrderMapper.INSTANCE.toDTO(orderEntity);
@@ -30,6 +34,16 @@ public class OrderService {
   private void validateInsert(OrderDTO order) {
     if (order.getId() != null) {
       throw new IllegalArgumentException("ID deve ser nulo ao inserir novo pedido.");
+    }
+  }
+
+  private void validate(OrderDTO order) throws IllegalArgumentException {
+    Set<ConstraintViolation<OrderDTO>> violations = Validation
+                                                              .buildDefaultValidatorFactory()
+                                                              .getValidator()
+                                                              .validate(order);
+    if (!violations.isEmpty()) {
+      throw new IllegalArgumentException("Valores inválidos");
     }
   }
 
--- backend/src/test/java/br/com/ufpr/das/order/OrderServiceTest.java
+++ backend/src/test/java/br/com/ufpr/das/order/OrderServiceTest.java
@@ -1,5 +1,52 @@
 package br.com.ufpr.das.order;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
+
+  @InjectMocks
+  private OrderService service;
+
+  @Mock
+  private OrderRepository orderRepository;
+
+  @Test
+  public void testInstance() {
+    assertNotNull(service);
+  }
+
+  @Test
+  public void testInsert() {
+    OrderDTO order = OrderDTOFactory.getOne("idNull");
+    Order orderEntity = OrderFaqctory.getOne("default");
+    when(orderRepository.save(ArgumentMatchers.any())).thenReturn(orderEntity);
+    OrderDTO result = service.insert(order);
+    verify(orderRepository).save(ArgumentMatchers.any());
+    assertNotNull(result);
+    assertEquals(result.getId(), orderEntity.getId());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsertIdNotNull() {
+    OrderDTO order = OrderDTOFactory.getOne("default");
+    service.insert(order);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInsertDateNull() {
+    OrderDTO order = OrderDTOFactory.getOne("dateNull");
+    service.insert(order);
+  }
 
 }
