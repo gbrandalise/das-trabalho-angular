9ef894966e9d2b596947ed3ee398c11260f93cb3
Hiago <hiaaugusto@hotmail.com>
2021-11-13 17:17:25 -0300

    criacao do produto

--- frontend/src/app/modules/product/product.module.ts
+++ frontend/src/app/modules/product/product.module.ts
@@ -3,12 +3,14 @@ import { ProductRoutingModule } from './product-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
