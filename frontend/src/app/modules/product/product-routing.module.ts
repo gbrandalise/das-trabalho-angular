bf82c56686c6f5b78126bf9b440d7048f02ef19f
joaofelipe <joaofelipe1294@gmail.com>
2021-11-20 16:37:59 -0300

    feat: add edit product.

--- frontend/src/app/modules/product/product-routing.module.ts
+++ frontend/src/app/modules/product/product-routing.module.ts
@@ -2,11 +2,16 @@ import { ProductsComponent } from './pages/products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './pages/product/product.component';
+import { ProductResolver } from './resolvers/product.resolver';
 
 const routes: Routes = [
+
   {
     path: 'register/:id',
-    component: ProductComponent
+    component: ProductComponent,
+    resolve: {
+      product: ProductResolver,
+    },
   },
   {path: 'register', component : ProductComponent,
 },
