1f6e89c855dcfc60369455d3ce6f6c09b54123f7
vanessavolochen <vanessa.volochen@gmail.com>
2022-03-06 13:05:49 -0300

    inclusão de produto mobile

--- mobile/lib/routes/app_routes.dart
+++ mobile/lib/routes/app_routes.dart
@@ -4,6 +4,7 @@ abstract class AppRoutes {
   static const PURCHASE_ORDER = '/purchase-order';
   static const PURCHASE_ORDER_REGISTER = '/purchase-order/register';
   static const PRODUCT = '/product';
+  static const PRODUCT_REGISTER = '/product/register';
   static const CLIENT = '/client';
   static const CLIENT_REGISTER = '/client/register';
 }
