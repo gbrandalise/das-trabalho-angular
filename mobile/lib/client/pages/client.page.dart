527962e48df8fdfa0dbb47b9b9e8b1f2e29aaf84
joaofelipe <joaofelipe1294@gmail.com>
2022-06-04 16:08:34 -0300

    feat: client form fields.
    Co-authored-by: André Gonçalves da Silva <andre.godasi@gmail.com>

--- mobile/lib/client/pages/client.page.dart
+++ mobile/lib/client/pages/client.page.dart
@@ -1,5 +1,5 @@
-
 import 'package:das_angular_mobile/menu/menu.component.dart';
+import 'package:das_angular_mobile/common/widgets/page-title.widget.dart';
 import 'package:flutter/material.dart';
 
 class ClientPage extends StatefulWidget {
@@ -12,6 +12,10 @@ class ClientPage extends StatefulWidget {
 }
 
 class _ClientPageState extends State<ClientPage> {
+  final _formKeyClient = GlobalKey<FormState>();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _cpfController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +31,42 @@ class _ClientPageState extends State<ClientPage> {
         ],
       ),
       drawer: const Menu(),
+      body: Column(
+        children: [
+          const PageTitle('Cadastrar Cliente'),
+          Form(
+            key: _formKeyClient,
+            child: Container(
+              margin: const EdgeInsetsDirectional.fromSTEB(10, 20, 10, 0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _firstNameController,
+                    decoration: const InputDecoration(
+                      enabled: true,
+                      labelText: 'Nome',
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _lastNameController,
+                    decoration: const InputDecoration(
+                      enabled: true,
+                      labelText: 'Sobrenome',
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _cpfController,
+                    decoration: const InputDecoration(
+                      enabled: true,
+                      labelText: 'CPF',
+                    ),
+                  ),
+                ],
+              ),
+            )
+          )
+        ]
+      )
     );
   }
 }
